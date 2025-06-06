9fd7ebb20f54bcbcc074ee724a0d3871fe12aff7
El Maco <marcus.myllyviita@gmail.com>
2025-06-07 02:25:01 +0300

    update port

--- src/main.rs
+++ src/main.rs
@@ -91,7 +91,7 @@ async fn main() {
         .layer(Extension(app_state))
         .layer(cors);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 
     let _ = eventloop_handle.await;
